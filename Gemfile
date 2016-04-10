1b411cb84b6f8e5f72d67f8cf27c750898d4000b
Tinus <vertelmy@gmail.com>
2016-04-10 14:13:28 +1200

    added angular-rails templates plus angular-route. Also basic controller setup

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jquery-rails'
 
 gem 'jbuilder', '~> 2.0'
 gem 'bower-rails', '~> 0.10.0'
+gem 'angular-rails-templates'
 gem 'foreman'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,12 @@
-var app = angular.module('angRec',[
+var app = angular.module('angRec',['templates', 'ngRoute', 'controllers'
 	]);
+
+app.config(['$routeProvider', function($routeProvider){$routeProvider.when('/', {
+	  templateUrl: "index.html",
+	  controller: 'RecipesController'
+    });
+  }
+]);
+
+controllers = angular.module('controllers', []);
+controllers.controller("RecipesController", ['$scope', function($scope) {}]);
