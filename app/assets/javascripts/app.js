0bbee3d78bea39f3abe28ed5cf20a756b2f2f7aa
Tinus <vertelmy@gmail.com>
2016-04-10 14:43:17 +1200

    Added Route Config, test data, controller config. Altered ttemp/indexx.html to enable search.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,12 +1,48 @@
 var app = angular.module('angRec',['templates', 'ngRoute', 'controllers'
 	]);
 
-app.config(['$routeProvider', function($routeProvider){$routeProvider.when('/', {
+// Route Config
+app.config(['$routeProvider', function($routeProvider){
+	return $routeProvider.when('/', {
 	  templateUrl: "index.html",
 	  controller: 'RecipesController'
     });
   }
 ]);
 
+// Test Data
+recipes = [
+  {
+  	id: 1,
+  	name: 'Baked Potato w/ Cheese'
+  },
+  {
+  	id: 2,
+  	name: 'Garlic Mashed Potatoes'
+  },
+  {
+  	id: 3,
+  	name: 'Potatoes Au Gratin'
+  },
+  {
+  	id: 4,
+  	name: 'Baked Brussel Sprouts'
+  }];
+
 controllers = angular.module('controllers', []);
-controllers.controller("RecipesController", ['$scope', function($scope) {}]);
+
+controllers.controller("RecipesController", ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
+	  var keywords;
+	  $scope.search = function(keywords){
+	  	return $location.path("/").search('keywords', keywords);
+	  };
+	  if ($routeParams.keywords) {
+	  	keywords = $routeParams.keywords.toLowerCase();
+	  	return $scope.recipes = recipes.filter(function(recipe){
+	  		return recipe.name.toLowerCase().indexOf(keywords) !== -1;
+	  	});
+	  } else {
+	  	return $scope.recipes = [];
+	  }
+	}
+]);
