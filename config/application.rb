301ff7f2bd38e1f6388004018959b5347bbcaeea
Tinus <vertelmy@gmail.com>
2016-04-20 17:48:02 +1200

    required angular and added Bootstrap to app.scss

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,12 @@ module AngRec
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # config to make glyphicons work
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap", "assets", "fonts")
+
+    config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
