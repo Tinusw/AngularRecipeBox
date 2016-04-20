7e34e3e3460225878bb6b070ff8352b8cc84647b
Tinus <vertelmy@gmail.com>
2016-04-20 18:00:50 +1200

    Added Bootstrap Sass. Plus smallest app ever.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module AngRec
 
     # config to make glyphicons work
     config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
-    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap", "assets", "fonts")
+    config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
