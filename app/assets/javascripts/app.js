a5abc3254d29bc2cf5c57fe8ec066e160ec243b0
Tinus <vertelmy@gmail.com>
2016-04-25 15:05:01 +1200

    included Angular dependencies, basics for routeProvider + js/templates/index created

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,2 +1,15 @@
 // Ang Module
-var app = angular.module('angRec',[]);
+var app = angular.module('angRec',['templates', 'ngRoute', 'controllers']);
+
+app.config([
+  '$routeProvider', function($routeProvider) {
+    return $routeProvider.when('/', {
+      templateUrl: "index.html",
+      controller: 'RecipesController'
+    });
+  }
+]);
+
+controllers = angular.module('controllers', []);
+
+controllers.controller("RecipesController", ['$scope', function($scope) {}]);
