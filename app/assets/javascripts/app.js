7093faffa9f6709d567be851f9ab27d3c25b7374
Tinus <vertelmy@gmail.com>
2016-04-25 15:19:05 +1200

    Added basic search using routeParams and Location. Index.html updated to impliment search

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,6 +10,44 @@ app.config([
   }
 ]);
 
+
+// Canned Recipe List
+recipes = [
+  {
+  	id: 1,
+  	name: 'Baked Potato w/ Cheese'
+  },
+  {
+  	id: 2,
+  	name: 'Garlic Mashed Potatoes'
+  },
+  {
+  	id: 3,
+  	name: 'Potatoes Au Gratin'
+  },
+  {
+  	id: 4,
+  	name: 'Baked Brussel Sprouts'
+  }
+ ];
+
 controllers = angular.module('controllers', []);
 
-controllers.controller("RecipesController", ['$scope', function($scope) {}]);
+controllers.controller("RecipesController", ['$scope', '$routeParams','$location', function($scope, $routeParams, $location){
+	  var keywords;
+
+	  //Search Function
+	  $scope.search = function(keywords) {
+	  	return $location.path("/").search('keywords', keywords);
+	  };
+
+	  if ($routeParams.keywords) {
+	  	keywords = $routeParams.keywords.toLowerCase();
+	  	return $scope.recipes = recipes.filter(function(recipe) {
+	  		return recipe.name.toLowerCase().indexOf(keywords) !== -1;
+	  	});
+	  } else {
+	  	return $scope.recipes = [];
+	  }
+  }
+]);
