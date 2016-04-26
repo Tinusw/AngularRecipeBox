21c7df882f48c077587205d40fd4f2a2cdf95612
Tinus <vertelmy@gmail.com>
2016-04-27 08:48:17 +1200

    capture injected service and return AJAX request

--- spec/javascripts/controllers/RecipesController_spec.js
+++ spec/javascripts/controllers/RecipesController_spec.js
@@ -1,5 +1,5 @@
 describe("RecipesController", function() {
-  var ctrl, location, resource, routeParams, scope, setupController;
+  var ctrl, location, resource, routeParams, scope, setupController, httpBackend;
 
   scope = null;
   ctrl = null;
@@ -7,13 +7,23 @@ describe("RecipesController", function() {
   routeParams = null;
   resource = null;
 
+  httpBackend = null;
+
 	setupController= function(keywords) {
-		return inject( function($location, $routeParams, $rootScope, $resource, $controller) {
+		return inject( function($location, $routeParams, $rootScope, $resource, $httpBackend, $controller) {
 			scope       = $rootScope.$new();
 			location    = $location;
 			resource    = $resource;
 			routeParams = $routeParams;
 			routeParams.keywords = keywords;
+
+			// capture injected service
+			httpBackend = $httpBackend;
+
+			if (results) {
+				request = new RegExp("\/recipes.*keywords=" + keywords);
+				httpBackend.expectGet(request).respond(results);
+			}
 			return ctrl = $controller('RecipesController', {
 				$scope: scope,
 				$location: location
