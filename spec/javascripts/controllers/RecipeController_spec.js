a057c32c49b3876ebd5e4f4eecac741043845a59
Tinus <vertelmy@gmail.com>
2016-04-28 10:54:47 +1200

    wrote more of the feature tests, got path working. Test currently fail

--- spec/javascripts/controllers/RecipeController_spec.js
+++ spec/javascripts/controllers/RecipeController_spec.js
@@ -18,12 +18,18 @@ describe("RecipeController", function(){
 			recipeExists = true;
 		}
 		return inject(function($location, $routeParams, $rootScope, $httpBackend, $controller) {
-			var location;
+			var location, request, results;
 			scope = $rootScope.$new();
 			location = $location;
 			httpBackend = $httpBackend;
 			routeParams = $routeParams;
 			routeParams.rec = recipeId;
+      
+      // Get Paths working
+			request = new RegExp("\/recipes/#{recipeId}");
+			results = recipeExists ? [200, fakeRecipe] : [404];
+			httpBackend.expectGET(request).respond(results[0], results[1]);
+
 			return ctrl = $controller('RecipeController', {
 				$scope: scope
 			});
@@ -36,4 +42,22 @@ describe("RecipeController", function(){
   	httpBackend.verifyNoOutstandingExpectation();
   	httpBackend.verifyNoOutstandingRequest();
   });
+  
+  // tests
+  return describe('controller initialization', function() {
+  	describe('recipe is found', function() {
+  		beforeEach(setupController());
+  		return it('loads the given recipe', function() {
+  			httpBackend.flush();
+  			return expect(scope.recipe).toEqualData(fakeRecipe);
+  		});
+  	});
+  		return describe('recipe is not found', function() {
+  			beforeEach(setupController(false));
+  			return it('loads the given recipe', function() {
+  				httpBackend.flush();
+  				return expect(scope.recipe).toBe(null);
+  			});
+  		});
+  	});
 });
