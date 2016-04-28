5ef9154d605a595213eb945a44673f032bf2809f
Tinus <vertelmy@gmail.com>
2016-04-29 10:32:10 +1200

    Found test issue. All Tests now pass in Teaspoon.

--- spec/javascripts/controllers/RecipeController_spec.js
+++ spec/javascripts/controllers/RecipeController_spec.js
@@ -24,17 +24,16 @@ describe("RecipeController", function(){
 			location = $location;
 			httpBackend = $httpBackend;
 			routeParams = $routeParams;
-			routeParams.rec = recipeId;
+			routeParams.recipeId = recipeId;
 			flash = _flash_;
       
       // Get Paths working
-			request = new RegExp("\/recipes/#{recipeId}");
-			results = recipeExists ? [200, fakeRecipe] : [404];
-			httpBackend.expectGET(request).respond(results[0], results[1]);
-
-			return ctrl = $controller('RecipeController', {
-				$scope: scope
-			});
+			request = new RegExp("\/recipes/" + recipeId);
+      results = recipeExists ? [200, fakeRecipe] : [404];
+      httpBackend.expectGET(request).respond(results[0], results[1]);
+      return ctrl = $controller('RecipeController', {
+        $scope: scope
+      });
 		});
 	};
   
@@ -47,20 +46,20 @@ describe("RecipeController", function(){
   
   // tests
   return describe('controller initialization', function() {
-  describe('recipe is found', function() {
-    beforeEach(setupController());
-    return it('loads the given recipe', function() {
-      httpBackend.flush();
-      return expect(scope.recipe).toEqualData(fakeRecipe);
+    describe('recipe is found', function() {
+      beforeEach(setupController());
+      return it('loads the given recipe', function() {
+        httpBackend.flush();
+        return expect(scope.recipe).toEqualData(fakeRecipe);
+      });
     });
-  });
-  return describe('recipe is not found', function() {
-    beforeEach(setupController(false));
-    return it('loads the given recipe', function() {
-      httpBackend.flush();
-      expect(scope.recipe).toBe(null);
-      return expect(flash.error).toBe("There is no recipe with ID " + recipeId);
+    return describe('recipe is not found', function() {
+      beforeEach(setupController(false));
+      return it('loads the given recipe', function() {
+        httpBackend.flush();
+        expect(scope.recipe).toBe(null);
+        return expect(flash.error).toBe("There is no recipe with ID " + recipeId);
+      });
     });
-  });
   });
 });
