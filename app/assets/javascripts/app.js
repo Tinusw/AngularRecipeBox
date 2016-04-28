3cf0e6aa215e74b4567b4113fe1266ce8001d133
Tinus <vertelmy@gmail.com>
2016-04-29 11:43:18 +1200

    init flash messages and expose bootstrap classes to angular-flash

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,7 +2,15 @@
 var app = angular.module('angRec',['templates', 'ngRoute', 'ngResource', 'controllers', 'angular-flash.service', 'angular-flash.flash-alert-directive']);
 
 app.config([
-  '$routeProvider', function($routeProvider) {
+  '$routeProvider', 'flashProvider', function($routeProvider, flashProvider) {
+    
+    // exposing the various bootstrap alerts to angular-flash
+    flashProvider.errorClassnames.push("alert-danger");
+    flashProvider.errorClassnames.push("alert-warning");
+    flashProvider.errorClassnames.push("alert-info");
+    flashProvider.errorClassnames.push("alert-success");
+
+    // routing
     return $routeProvider.when('/', {
       templateUrl: "index.html",
       controller: 'RecipesController'
