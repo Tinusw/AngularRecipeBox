556584fdb8cc0798228095597594f15d11bd7f8b
Tinus <vertelmy@gmail.com>
2016-04-29 12:07:11 +1200

    forgot to add routes to routes.rb...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :create, :update, :destroy]
 
 
 end
