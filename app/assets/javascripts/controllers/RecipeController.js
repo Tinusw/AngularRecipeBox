08f691662b00c5aee5a1c850503804d7587dfbec
Tinus <vertelmy@gmail.com>
2016-04-29 12:46:29 +1200

    wrote FrontEnd controller changes for front end testing. Not complete

--- app/assets/javascripts/controllers/RecipeController.js
+++ app/assets/javascripts/controllers/RecipeController.js
@@ -6,10 +6,18 @@ controllers.controller("RecipeController", ['$scope', '$routeParams', '$resource
 	{
 		recipeId: "@id",
 		format: 'json'
-	});
-  
+	}, {
+    'save': {
+     method:'PUT' 
+   },
+    'create': {
+     method: 'POST' 
+   }
+  });
+
   // Making http calls out to tests
-	return Recipe.get({
+  if ($routeParams.recipeId) {
+	Recipe.get({
       recipeId: $routeParams.recipeId
     }, (function(recipe) {
       return $scope.recipe = recipe;
@@ -17,8 +25,42 @@ controllers.controller("RecipeController", ['$scope', '$routeParams', '$resource
       $scope.recipe = null;
       return flash.error = "There is no recipe with ID " + $routeParams.recipeId;
     }));
-    return $scope.back = function() {
+  } else {
+    $scope.recipe = {};
+  }
+
+  // FUNCTIONS
+  $scope.back = function() {
+    return $location.path("/");
+  };
+  $scope.edit = function() {
+    return $location.path("/recipes/" + $scope.recipe.id + "/edit");
+  };
+  $scope.cancel = function() {
+    if ($scope.recipe.id) {
+      return $location.path("/recipes/" + $scope.recipe.id);
+    } else {
       return $location.path("/");
+    }
+  };
+  $scope.save = function() {
+    var onError;
+    onError = function(_httpResponse) {
+      return flash.error = "Something went wrong";
+    };
+    if ($scope.recipe.id) {
+      return $scope.recipe.$save((function() {
+        return $location.path("/recipes/" + $scope.recipe.id);
+      }), onError);
+    } else {
+      return Recipe.create($scope.recipe, (function(newRecipe) {
+        return $location.path("/recipes/" + newRecipe.id);
+      }), onError);
+    }
+  };
+    return $scope["delete"] = function() {
+    $scope.recipe.$delete();
+      return $scope.back();
     };
   }
 ]);
